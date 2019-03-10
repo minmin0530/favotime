d26a8ccbdcbdd33c0c36a40a38151e2d770bf9ed
izumi_yoshiki <izumiyoshiki@icloud.com>
2019-03-10 11:34:21 +0900

    post送信を削除

--- favotime/ViewController.swift
+++ favotime/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     let realm = try! Realm()
     var contentArray = try! Realm().objects(Content.self).sorted(byKeyPath: "date", ascending: false)
 
-    var manager = SocketManager(socketURL: URL(string: "http://localhost:3000")!, config: [.forceWebsockets(true)])
+    var manager = SocketManager(socketURL: URL(string: "https://ai6.jp")!, config: [.forceWebsockets(true)])
     var socket: SocketIOClient! //= self.manager.defaultSocket
     var sendFlag :Bool = false
     var serveFlag :Bool = false
@@ -27,20 +27,52 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.dataSource = self
         socket = self.manager.defaultSocket
 
+        /*
+        let url = URL(string: "https://ai6.jp")
+        var request = URLRequest(url: url!)
+        // POSTを指定
+        request.httpMethod = "POST"
+        // POSTするデータをBodyとして設定
+        request.httpBody = "userName=test&password=test".data(using: .utf8)
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if error == nil, let data = data, let response = response as? HTTPURLResponse {
+                // HTTPヘッダの取得
+                print("Content-Type: \(response.allHeaderFields["Content-Type"] ?? "")")
+                // HTTPステータスコード
+                print("statusCode: \(response.statusCode)")
+                print(String(data: data, encoding: .utf8) ?? "")
+            }
+            }.resume()
+        */
+        
         test()
     }
+//    let json = "[{\"a\":\"1\"},{\"b\":\"2\"}]".data(using: .utf8)!
+//    let decoder = JSONDecoder()
 
     func test() {
-        manager = SocketManager(socketURL: URL(string: "http://localhost:3000")!, config: [.forceWebsockets(true)])
+        manager = SocketManager(socketURL: URL(string: "https://ai6.jp")!, config: [.forceWebsockets(true)])
         socket = self.manager.defaultSocket
-        socket.on("connect") { data, ack in
+        socket.on("connect") { data0, ack in
             print("socket connected")
             print("send message")
 //            if self.sendFlag == true {
 //                self.sendFlag = false
-                self.socket.emit("from_client", self.temp)
+
+            var objs: [String:String] = [:]
+            do {
+                objs = try JSONDecoder().decode([String:String].self, from: "{\"userName\":\"1\",\"password\":\"2\"}".data(using: .utf8)!)
+            } catch {
+                
+            }
+            self.socket.emit("login", objs);
+            //    self.socket.emit("from_client", self.temp)
                 self.tableView.reloadData()
 //            }
+        }
+        socket.on("logined") { data, ack in
+            self.socket.emit("from_client", "aaa");
         }
         socket.on("from_server") { data, ack in
             if let msg = data[0] as? String {
